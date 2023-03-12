3f1215a42a1c9535102ed736ac6942a549e855db
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2023-03-12 17:48:55 -0600

    subiendo ejercicio 1 de la sesion 8

---
+++ Exercise1S8/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+            double ancho, altura;
+
+            Console.WriteLine("Ingrese el ancho del rectángulo:");
+            ancho = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la altura del rectángulo:");
+            altura = double.Parse(Console.ReadLine());
+
+            double perimetro = 2 * (ancho + altura);
+            double area = ancho * altura;
+            double diagonal = Math.Sqrt(Math.Pow(ancho, 2) + Math.Pow(altura, 2));
+
+            Console.WriteLine("El perímetro del rectángulo es: " + perimetro);
+            Console.WriteLine("El área del rectángulo es: " + area);
+            Console.WriteLine("La diagonal del rectángulo es: " + diagonal);
+   
