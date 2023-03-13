0049ed0b65653af8bd3756ba199f6a9dfceb7799
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2023-03-12 18:06:14 -0600

    Se suben ejercicios de la sesion 9

---
+++ Exercise1S9/Program.cs
@@ -0,0 +1,104 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace EjercicioCasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear un pequeño apartamento
+            PequenoApartamento miApartamento = new PequenoApartamento();
+
+            // Crear una persona
+            Persona yo = new Persona("Xiomara", miApartamento);
+
+            // Mostrar los datos de la persona
+            yo.ShowData();
+        }
+    }
+
+    // Definir la clase Casa
+    class Casa
+    {
+        private double area;
+
+        public Casa(double area)
+        {
+            this.area = area;
+        }
+
+        public double GetArea()
+        {
+            return area;
+        }
+
+        public void SetArea(double area)
+        {
+            this.area = area;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("Soy una casa, mi área es de " + area + " m2.");
+        }
+
+        public Puerta GetDoor()
+        {
+            return new Puerta("marrón");
+        }
+    }
+
+    // Definir la clase Puerta
+    class Puerta
+    {
+        private string color;
+
+        public Puerta(string color)
+        {
+            this.color = color;
+        }
+
+        public string GetColor()
+        {
+            return color;
+        }
+
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("Soy una puerta, mi color es " + color + ".");
+        }
+    }
+
+    // Definir la clase PequenoApartamento como una subclase de Casa
+    class PequenoApartamento : Casa
+    {
+        public PequenoApartamento() : base(50.0)
+        {
+        }
+    }
+
+    // Definir la clase Persona
+    class Persona
+    {
+        private string nombre;
+        private Casa casa;
+
+        public Persona(string nombre, Casa casa)
+        {
+            this.nombre = nombre;
+            this.casa = casa;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine(nombre + " vive en una casa de " + casa.GetArea() + " m2.");
+            casa.ShowData();
+            casa.GetDoor().ShowData();
+        }
+    }
+}
---
+++ Exercise2S9/Program.cs
@@ -0,0 +1,103 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace TextDisplay
+{
+    class ScreenText
+    {
+        private int x;
+        private int y;
+        private string text;
+
+        public ScreenText(int x, int y, string text)
+        {
+            this.x = x;
+            this.y = y;
+            this.text = text;
+        }
+
+        public void SetX(int x)
+        {
+            this.x = x;
+        }
+
+        public void SetY(int y)
+        {
+            this.y = y;
+        }
+
+        public void SetText(string text)
+        {
+            this.text = text;
+        }
+
+        public void Display()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(text);
+        }
+    }
+
+    class CenteredText : ScreenText
+    {
+        public CenteredText(int y, string text) : base(0, y, text)
+        {
+            SetX((Console.WindowWidth - text.Length) / 2);
+        }
+    }
+
+    class FramedText : CenteredText
+    {
+        private int startY;
+
+        public FramedText(int startY, string text) : base(startY + 1, text)
+        {
+            this.startY = startY;
+        }
+
+        public override void Display()
+        {
+            int length = GetLength();
+            int x = (Console.WindowWidth - length) / 2;
+            int y = startY;
+            Console.SetCursorPosition(x, y);
+            Console.Write("┌" + new string('─', length - 2) + "┐");
+            for (int i = 0; i < GetHeight(); i++)
+            {
+                y++;
+                Console.SetCursorPosition(x, y);
+                Console.Write("│" + new string(' ', length - 2) + "│");
+            }
+            y++;
+            Console.SetCursorPosition(x, y);
+            Console.Write("└" + new string('─', length - 2) + "┘");
+            base.Display();
+        }
+
+        private int GetLength()
+        {
+            return GetText().Length + 2;
+        }
+
+        private int GetHeight()
+        {
+            return 3;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScreenText screenText = new ScreenText(10, 10, "Hello, world!");
+            screenText.Display();
+
+            CenteredText centeredText = new CenteredText(15, "Este texto está centrado horizontalmente.");
+            centeredText.Display();
+
+            FramedText framedText = new FramedText(20, "Este texto está centrado y enmarcado.");
+            framedText.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
