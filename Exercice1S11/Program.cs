f5ceec66a8eccd766c5dac8f411b0f26fa00bb78
txiomaraamador <xiomaralizethamadoraguilera@gmail.com>
2023-03-15 13:27:29 -0600

    Se suben ejercicios de la sesion 11

---
+++ Exercice1S11/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+    
+int[] arr = {5, 2, 7, 1, 3};
+        
+Console.WriteLine("Array antes de ordenar:");
+
+foreach (int i in arr) {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+
+int n = arr.Length;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+Console.WriteLine("Array después de ordenar:");
+
+foreach (int i in arr) {
+            Console.Write(i + " ");
+        }
+        Console.WriteLine();
+
---
+++ Exercice2S11/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+    
+int[] arr = {1, 3, 5, 7, 9, 11, 13};
+int x = 7;
+
+static int BusquedaBinaria(int[] arr, int x) {
+    int inicio = 0;
+    int fin = arr.Length - 1;
+
+    while (inicio <= fin) {
+        int medio = inicio + (fin - inicio) / 2;
+
+        if (arr[medio] == x) {
+            return medio;
+        }
+
+        if (arr[medio] < x) {
+            inicio = medio + 1;
+        } else {
+            fin = medio - 1;
+        }
+    }
+        return -1;
+    }
+
+int resultado = BusquedaBinaria(arr, x);
+
+if (resultado == -1) {
+    Console.WriteLine("El elemento no se encuentra en la matriz.");
+} else {
+    Console.WriteLine("El elemento se encuentra en el índice " + resultado);
+    }
+    
+
+
+
